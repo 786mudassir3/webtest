a9bced7d34b0ebd10ad263ee910da120abcdd5a4
786mudassir3 <151738891+786mudassir3@users.noreply.github.com>
2026-03-06 15:18:44 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,94 @@
+# Python Data Types
+# =================
+#   - String (str)  -> anything(A-Z,a-z,0-9,all special characters) inside "" or ''
+#                   -> name, address, email
+
+#   - Integer (int) -> numeric -> non-decimal number
+#   - Float (float) -> numeric -> decimal number
+
+# Variables -> to store data
+# ---------
+name = "Ibaad Abbasi"
+#print(name)
+#print(name)
+#print(name)
+#print(name)
+
+x = 10
+y = 20
+z = 20.4
+#print(x+y)
+#print(x*y)
+
+# type() - return data type
+
+#dt1 = type(name)
+#print(dt1)
+
+#print(type(name))
+#print(type(x))
+#print(type(z))
+
+student = name
+#print(student)
+
+i = 10
+i = i + 10
+#print(i)
+# -------------------------------------------------------------------------
+
+name = "Amit Gurjar"
+age = 20
+city = "Bhopal"
+
+#print(name)
+#print(age)
+#print(city)
+#print(name,age,city)
+#print("Student Name is",name)
+#print("Student Age is",age)
+#print("Student City is",city)
+#print("Name is",name,",","Age is",age,",","City is",city)
+#print("Name is",name,", Age is",age,", City is",city)
+#print("Name is",name,"\n","Age is",age,"\n","City is",city)
+print("Name is",name,"\nAge is",age,"\nCity is",city)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ demo.py
@@ -0,0 +1,22 @@
+#print('Ibad Abbasi')
+"""
+print("He is a Data Analytics Student")
+print("Python")
+print("Data Science with AIML")
+"""
+
+# Escape Character - \
+# \n    : next line
+# \t    : tab
+
+#print("First Line \nSecond Line")
+#print("First Line \nSecond Line \n\nThird Line")
+
+#print("Ibaad Abbasi")
+#print("Ibaad \tAbbasi")
+#print("Ibaad \t\tAbbasi")
+#print("Python is a 'Programming' Language")
+#print('Python is a "Programming" Language')
+#print("Python is a \"Programming\" Language")
+print("z\\b")
+
